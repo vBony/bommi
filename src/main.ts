52ffbf961edbc6d58b3577307424c3a23c6f4edc
MATHEUS <matheus8142@hotmail.com>
2021-08-31 20:38:06 -0300

    (#3) - Enviando dados pro back-end

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 createApp(App).use(store).use(router).mount('#app')
