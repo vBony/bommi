745839b9a43c3a1d8db33fdf95afffcdef36ae16
MATHEUS <matheus8142@hotmail.com>
2021-09-08 21:00:09 -0300

    Merge develop in #6

--- src/mixins/DocumentMixin.ts
+++ src/mixins/DocumentMixin.ts
@@ -1,37 +1,39 @@
+import System from '@/entities/System';
+import $ from 'jquery'
+import axios from 'axios';
+
 class DocumentMixin {
-    // updateMessageInputs(entitie){
-        
-    // }
-
-    string_to_slug(str:string):string {
-        str = str.replace(/^\s+|\s+$/g, ""); // trim
-        str = str.toLowerCase();
-      
-        // remove accents, swap ñ for n, etc
-        const from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
-        const to = "aaaaaaeeeeiiiioooouuuunc------";
-      
-        for (let i = 0, l = from.length; i < l; i++) {
-          str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
-        }
-      
-        str = str
-          .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
-          .replace(/\s+/g, "-") // collapse whitespace and replace by -
-          .replace(/-+/g, "-"); // collapse dashes
-      
-        return str;
-    }
-
-    getUrlServer(){
-      const url = window.location.hostname;
-      
-      if(url != 'localhost'){
-        return 'https://api.ubarber.com.br/'
-      }else {
-        return 'http://localhost:8012/'
+  public System = new System()
+  
+  string_to_slug(str:string):string {
+      str = str.replace(/^\s+|\s+$/g, ""); // trim
+      str = str.toLowerCase();
+    
+      // remove accents, swap ñ for n, etc
+      const from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+      const to = "aaaaaaeeeeiiiioooouuuunc------";
+    
+      for (let i = 0, l = from.length; i < l; i++) {
+        str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
       }
+    
+      str = str
+        .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+        .replace(/\s+/g, "-") // collapse whitespace and replace by -
+        .replace(/-+/g, "-"); // collapse dashes
+    
+      return str;
+  }
+
+  getUrlServer(){
+    const url = window.location.hostname;
+    
+    if(url != 'localhost'){
+      return 'https://api.ubarber.com.br/'
+    }else {
+      return 'http://localhost:8012/'
     }
+  }
 }
 
 export default DocumentMixin
